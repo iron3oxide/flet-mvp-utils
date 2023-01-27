e4b07972110d92085b1cd1e16f094dd1aab19297
jh <jason.hottelet@tuta.io>
2023-01-28 00:52:30 +0100

    refactor(view): Make MvpView inherit from flet.View

--- flet_mvp_utils/view.py
+++ flet_mvp_utils/view.py
@@ -1,5 +1,5 @@
 from dataclasses import asdict
-from typing import Protocol
+from typing import List, Optional, Protocol
 
 import flet as ft
 
@@ -8,8 +8,39 @@ class DataclassProtocol(Protocol):
     __dataclass_fields__: dict
 
 
-class MvpView:
-    def __init__(self, ref_map: dict[str, ft.Ref]) -> None:
+class MvpView(ft.View):
+    def __init__(
+        self,
+        ref_map: dict[str, ft.Ref],
+        *,
+        route: Optional[str] = None,
+        controls: Optional[List[ft.Control]] = None,
+        appbar: Optional[ft.AppBar] = None,
+        floating_action_button: Optional[ft.FloatingActionButton] = None,
+        navigation_bar: Optional[ft.NavigationBar] = None,
+        vertical_alignment: ft.MainAxisAlignment = ft.MainAxisAlignment.NONE,
+        horizontal_alignment: ft.CrossAxisAlignment = ft.CrossAxisAlignment.NONE,
+        spacing: ft.OptionalNumber = None,
+        padding: ft.PaddingValue = None,
+        bgcolor: Optional[str] = None,
+        scroll: Optional[ft.ScrollMode] = None,
+        auto_scroll: Optional[bool] = None,
+    ) -> None:
+        super().__init__(
+            route,
+            controls,
+            appbar,
+            floating_action_button,
+            navigation_bar,
+            vertical_alignment,
+            horizontal_alignment,
+            spacing,
+            padding,
+            bgcolor,
+            scroll,
+            auto_scroll,
+        )
+
         self.ref_map = ref_map
 
     def render(self, model: DataclassProtocol):
