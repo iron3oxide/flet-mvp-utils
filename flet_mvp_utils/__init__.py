3077c71d802dd933450cd19813234c499acfdb7d
jh <jason.hottelet@tuta.io>
2023-01-30 21:50:08 +0100

    feat(project): Expose presenter and protocols

--- flet_mvp_utils/__init__.py
+++ flet_mvp_utils/__init__.py
@@ -2,4 +2,6 @@ from flet_mvp_utils.datasource import MvpDataSource
 from flet_mvp_utils.error import ErrorMessage
 from flet_mvp_utils.model import MvpModel
 from flet_mvp_utils.observable import Observable
+from flet_mvp_utils.presenter import MvpPresenter
+from flet_mvp_utils.protocols import MvpPresenterProtocol, MvpViewProtocol
 from flet_mvp_utils.view import MvpView
