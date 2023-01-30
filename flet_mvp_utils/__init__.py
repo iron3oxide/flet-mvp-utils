f7ab0bb03c2ef45b65e44d4e3f49b371183585f6
jh <jason.hottelet@tuta.io>
2023-01-30 15:27:23 +0100

    feat(project): expose new classes

--- flet_mvp_utils/__init__.py
+++ flet_mvp_utils/__init__.py
@@ -1,2 +1,5 @@
+from flet_mvp_utils.datasource import MvpDataSource
+from flet_mvp_utils.error import ErrorMessage
+from flet_mvp_utils.model import MvpModel
 from flet_mvp_utils.observable import Observable
 from flet_mvp_utils.view import MvpView
