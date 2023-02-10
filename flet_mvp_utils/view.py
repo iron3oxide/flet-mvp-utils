f6d288e7679cb6b39c6a73260b5acea99b05c718
jh <jason.hottelet@tuta.io>
2023-02-11 00:17:46 +0100

    fix(view): get message of ErrorMessage object, update page only once

--- flet_mvp_utils/view.py
+++ flet_mvp_utils/view.py
@@ -42,20 +42,27 @@ class MvpView(ft.View):
         self.ref_map = ref_map
 
     def render(self, model: BaseModel) -> None:
+        page: ft.Page | None = None
         model_map = model.dict()
+
         for variable_name, ref in self.ref_map.items():
 
+            model_field_content = model_map[variable_name]
             control_attribute_name = "value"
             if not hasattr(ref.current, control_attribute_name):
                 control_attribute_name = "text"
-            if isinstance(model_map[variable_name], ErrorMessage):
+            if isinstance(model_field_content, ErrorMessage):
                 control_attribute_name = "error_text"
+                model_field_content = model_field_content.message
 
-            control_attribute_value = getattr(ref.current, control_attribute_name)
+            control_attribute_content = getattr(ref.current, control_attribute_name)
 
-            if model_map[variable_name] == control_attribute_value:
+            if model_field_content == control_attribute_content:
                 continue
-            setattr(ref.current, control_attribute_name, model_map[variable_name])
+            setattr(ref.current, control_attribute_name, model_field_content)
 
-            if ref.current.page:
-                ref.current.update()
+            if not page:
+                page = ref.current.page
+
+        if page:
+            page.update()
