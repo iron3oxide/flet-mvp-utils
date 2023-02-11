b2c3c21f3b05486ece228c6b08958583c2f514e6
jh <jason.hottelet@tuta.io>
2023-02-12 00:58:08 +0100

    refactor(presenter): Disallow instantiation of MvpPresenter, remove debugging code

--- flet_mvp_utils/presenter.py
+++ flet_mvp_utils/presenter.py
@@ -1,9 +1,7 @@
 from dataclasses import dataclass
 
 from flet_mvp_utils.datasource import MvpDataSource
-from flet_mvp_utils.model import MvpModel
-from flet_mvp_utils.protocols import MvpPresenterProtocol, MvpViewProtocol
-from flet_mvp_utils.view import MvpView
+from flet_mvp_utils.protocols import MvpViewProtocol
 
 
 @dataclass
@@ -12,6 +10,8 @@ class MvpPresenter:
     view: MvpViewProtocol
 
     def __post_init__(self) -> None:
+        if self.__class__ == MvpPresenter:
+            raise TypeError("Cannot instantiate abstract class.")
         self.data_source.register(self.update_view)
 
     def build(self) -> None:
@@ -19,36 +19,3 @@ class MvpPresenter:
 
     def update_view(self) -> None:
         self.view.render(self.data_source.current_model)
-
-
-class MockM(MvpModel):
-    age: int = 0
-
-
-class MockDS(MvpDataSource):
-    current_model = MockM()
-
-    def mock(self):
-        print(self.current_model.age)
-
-
-class MockV(MvpView):
-    ref_map = {}
-
-    def build(self, presenter: MvpPresenterProtocol) -> None:
-        ...
-
-
-@dataclass
-class MockP(MvpPresenter):
-    data_source: MockDS
-    view: MockV
-
-    def test(self):
-        self.data_source.mock()
-
-
-m = MockDS(route_params={})
-v = MockV()
-p = MockP(m, v)
-p.test()
